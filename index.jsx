14694d01791c1b1192a12c04f095aff1b7fc9650
Paul <paul@k15t.com>
2019-12-08 11:05:01 +0100

    Improve design

--- index.jsx
+++ index.jsx
@@ -8,6 +8,7 @@ import Backend from 'react-dnd-html5-backend';
 import _ from 'lodash/fp';
 import _formatDecimal from 'format-decimal';
 
+
 function formatDecimal(number) {
     return _formatDecimal(number).split(',')[0];
 }
@@ -65,13 +66,13 @@ const Selection = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    overflow: scroll;   
+    overflow: scroll;
 `;
 
 const Gallery = styled.div`
-    border-top: 1px solid #000;
     height: 300px;
     padding: 20px;
+    padding-left: 40px;
     display: flex;
     align-items: center;
     width: 100%;
@@ -79,9 +80,12 @@ const Gallery = styled.div`
 `;
 
 const InfoBox = styled.div`
-    width: 400px;
-    border-right: 1px solid #000;
-    padding: 20px;
+    width: 450px;
+    border-right: 1px solid #aaa;
+    padding: 40px;
+    margin-bottom: 40px;
+    margin-left: 20px;
+    position: relative;
 `;
 
 const Comparison = styled.div`
@@ -94,36 +98,119 @@ const InfoProperty = styled.div`
     font-size: 20px;
     margin-bottom: 20px;
     line-height: 1.3em;
+    color: #a88a49
+    font-weight: bold;
 `;
 
 const InfoLabel = styled.div`
-    font-size: 16px;
+    font-size: 20px;
     line-height: 1.3em;
+    color: #a88a49;    
 `;
 
 const InfoHeadline = styled.div`
     font-size: 25px;
+    font-weight: bold;
     margin-bottom: 20px;
     line-height: 1.3em;
+    color: #a88a49;
 `;
 
+
+const AppHeader = styled.div`
+    font-size: 31.25px;
+    padding: 20px;
+`
 
 const Button = styled.button`
-    border: 1px solid grey;
-    border-radius: 5px;    
+    border: 1px solid #a88a49;;
+    color: #a88a49;
+    border-radius: 2px;    
     outline: 0;
-    background: #fff;
-    font-size: 16px;
-    padding: 5px;    
+    background: #fff;    
+    font-size: 20px;
+    padding: 10px;    
+    font-family: Roboto;  
+`;
+
+const Center = styled.div`    
+    display: flex;    
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    bottom: 20px;
+    left: 0;
+    right: 0;
+`;
+
+const Search = styled.div`
+    display: flex;  
+    height: 100px;
+    align-items: center;
+    padding: 20px;
+    margin-top: 20px;
+    margin-left: 20px;
+`;
+
+const SearchInput = styled.input`
+    font-size: 20px;
+    border: 1px solid #a88a49;
+    padding: 10px;
+    outline: 0;  
+    width: 500px;  
+`;
+
+const SearchCount = styled.div`
+    margin-left: 20px;
+`;
+
+const Line = styled.div`
+    border-bottom: 1px solid #aaa;
+    margin: 0 40px;
+    margin-top: 20px; 
+`;
+
+const DropHelpMessage = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const DropHelpMessageInner = styled.div`
+    border: 1px dashed #aaa;    
+    font-size: 20px;
+    width: 700px;
+    height: 300px;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;    
 `;
 
 
-const Center = styled.div`
-    margin-top: 40px;
-    display: flex;
-    width: 100%;
-    align-items: center;
-    justify-content: center;
+const Logo = styled.div`
+    background: url('logo.svg');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100px;
+    width: 150px;
+    margin-right: 40px;    
+`;
+
+const EraInfo = styled.div`
+    padding: 20px 0 20px 60px;    
+`;
+
+const EraInfoHeader = styled.div`
+    font-size: 20px;    
+`;
+
+const EraInfoValue = styled.div`
+    font-size: 20px;
+    font-weight: bold;
 `;
 
 class App extends React.Component {
@@ -132,11 +219,13 @@ class App extends React.Component {
         super();
 
         this.state = {
-            focusedObjectId: exhibitionObjects[0].id,
-            selectedObjects: [exhibitionObjects[0]]
+            focusedObjectId: null, //exhibitionObjects[0].id,
+            selectedObjects: [], //[exhibitionObjects[0]],
+            search: '',
+            currentEra: exhibitionObjects[0].era
         };
 
-        _.bindAll(['handleDrop', 'handleFocusObject', 'removeFocusedObject'], this);
+        _.bindAll(['handleDrop', 'handleFocusObject', 'removeFocusedObject', 'handleScroll', 'updateSearch'], this);
     }
 
     handleDrop(object) {
@@ -157,51 +246,110 @@ class App extends React.Component {
 
             return {
                 selectedObjects: newSelectedObjects,
-                focusedObjectId: newSelectedObjects[0] ?  newSelectedObjects[0].id : null
+                focusedObjectId: newSelectedObjects[0] ? newSelectedObjects[0].id : null
             };
         });
     }
 
+    handleScroll(evt) {
+        this.updateEra();
+    }
+
+    updateEra() {
+        const scrolledToId = document.elementFromPoint(50, 250).id;
+        const object = exhibitionObjects.find(({ id }) => id === scrolledToId);
+
+        if (object) {
+            this.setState({ currentEra: object.era });
+        }
+    }
+
+    updateSearch (search) {
+        this.setState({ search });
+
+        setTimeout(() => {
+            this.updateEra();
+        })
+    }
+
     render() {
-        const { selectedObjects, focusedObjectId } = this.state;
+        const { selectedObjects, focusedObjectId, search, currentEra } = this.state;
         const focusedObject = exhibitionObjects.find(({ id }) => id === focusedObjectId);
+        const filteredExhibitionObjects = _.filter((object) => object.name.indexOf(search) !== -1, exhibitionObjects);
+        const resultCount = filteredExhibitionObjects.length;
 
         return (
             <DndProvider backend={Backend}>
                 <Content>
+                    <Search>
+                       <Logo/>
+
+                        <SearchInput
+                            onChange={((evt) => this.updateSearch(evt.target.value))}
+                            value={search}
+                            placeholder={'Stichwort eingeben'}
+                        />
+
+                        <SearchCount>{resultCount} {resultCount === 1 ? 'gefundenes Objekt' : 'gefundenen Objekte'}</SearchCount>
+                    </Search>
+
+                    <Line/>
+
+                    <EraInfo>
+                        <EraInfoValue>
+                            {currentEra}
+                        </EraInfoValue>
+                    </EraInfo>
+
+                    <Gallery onScroll={this.handleScroll}>
+                        {_.flow(
+                            _.filter((object) => !selectedObjects.find(({ id }) => {
+                                return object.id === id;
+                            })),
+                            _.map((object) => (
+                                <ImageView
+                                    key={object.id}
+                                    object={object}
+                                />
+                            ))
+                        )(filteredExhibitionObjects)}
+                    </Gallery>
+                    <Line/>
                     <Comparison>
-                        <InfoBox>
-                            {focusedObject && (
-                                <>
 
-                                    <InfoLabel>{focusedObject.inventoryId}</InfoLabel>
-                                    <InfoHeadline>{focusedObject.name}</InfoHeadline>
+                        {(selectedObjects.length > 0) && (
+                            <InfoBox>
+                                {focusedObject && (
+                                    <>
+                                        <InfoLabel>{focusedObject.inventoryId}</InfoLabel>
+                                        <InfoHeadline>{focusedObject.name}</InfoHeadline>
 
 
-                                    <InfoLabel>Material</InfoLabel>
-                                    <InfoProperty>{focusedObject.materials.join(', ')}</InfoProperty>
+                                        <InfoLabel>Material</InfoLabel>
+                                        <InfoProperty>{focusedObject.materials.join(', ')}</InfoProperty>
 
-                                    <InfoLabel>Technik</InfoLabel>
-                                    <InfoProperty>{focusedObject.technology.join(', ')}</InfoProperty>
+                                        <InfoLabel>Technik</InfoLabel>
+                                        <InfoProperty>{focusedObject.technology.slice(0, 4).join(', ')}</InfoProperty>
 
-                                    <InfoLabel>Datierung</InfoLabel>
-                                    <InfoProperty>
-                                        {focusedObject.era}<br/>
-                                        {focusedObject.date}
-                                    </InfoProperty>
+                                        <InfoLabel>Datierung</InfoLabel>
+                                        <InfoProperty>
+                                            {focusedObject.era}<br/>
+                                            {focusedObject.date}
+                                        </InfoProperty>
 
-                                    <InfoLabel>Fundort</InfoLabel>
-                                    <InfoProperty>{focusedObject.location}</InfoProperty>
+                                        <InfoLabel>Fundort</InfoLabel>
+                                        <InfoProperty>{focusedObject.location}</InfoProperty>
 
-                                    <Center>
-                                        <Button onClick={() => this.removeFocusedObject()}>
-                                            Objekt zurücklegen
-                                        </Button>
-                                    </Center>
+                                        <Center>
+                                            <Button onClick={() => this.removeFocusedObject()}>
+                                                Objekt zurücklegen
+                                            </Button>
+                                        </Center>
 
-                                </>
-                            )}
-                        </InfoBox>
+                                    </>
+                                )}
+                            </InfoBox>
+                        )}
 
                         <SelectionView
                             focusedObjectId={focusedObjectId}
@@ -210,17 +358,6 @@ class App extends React.Component {
                             onFocusObject={this.handleFocusObject}
                         />
                     </Comparison>
-                    <Gallery>
-                        {_.flow(
-                            _.map((object) => (
-                                <ImageView
-                                    key={object.id}
-                                    object={object}
-                                />
-                            )),
-                            _.filter((object) => !selectedObjects.find(({ id }) => object.id === id))
-                        )(exhibitionObjects)}
-                    </Gallery>
                 </Content>
             </DndProvider>
         );
@@ -229,7 +366,7 @@ class App extends React.Component {
 
 const SelectionPreviewImage = styled.img`
     max-height: 500px;
-    opacity: ${({isFocused}) => isFocused ? '1' : '0.5'}
+    opacity: ${({ isFocused }) => isFocused ? '1' : '0.5'}
 `;
 
 function SelectionPreview({ object, onClick, isFocused }) {
@@ -269,13 +406,23 @@ function SelectionView({ onDrop, onFocusObject, selectedObjects, focusedObjectId
                     isFocused={focusedObjectId === object.id}
                 />
             ), selectedObjects)}
+
+            {selectedObjects.length === 0 && (
+                <DropHelpMessage>
+                    <DropHelpMessageInner>
+                        <img src='vase.svg'/>
+
+                        <div style={{paddingLeft: '20px'}}>Ziehe ein Fundstück aus dem Zeitstrahl auf den Untersuchungstisch um es genauer anzuschauen</div>
+                    </DropHelpMessageInner>
+                </DropHelpMessage>
+            )}
         </Selection>
     );
 }
 
 const Image = styled.img`
-    height: 200px;
-    opacity: ${({ isDragging }) => isDragging ? 0.5 : 1};
+    height: 150px;
+    opacity: ${({ isDragging }) => isDragging ? 0.5 : 1};    
 `;
 
 
@@ -287,10 +434,17 @@ function ImageView({ object }) {
         })
     });
 
+    let ref;
+
     return (
-        <div ref={dragRef}>
+        <div ref={(r) => {
+            ref = r;
+            dragRef(r);
+        }}>
             <Image isDragging={isDragging}
-                   src={object.previewUrl}/>
+                   src={object.previewUrl}
+                   id={object.id}
+            />
         </div>
     );
 }
